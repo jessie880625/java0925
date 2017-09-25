e5e123d9e15124d46edeccab373b791f2da4a2fc
106022044 <106022044@live.asia.edu.tw>
2017-09-25 15:17:00 +0800

    Third Commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
     public  static void main(String[] args){
         System.out.println("第一個Java程式");
         System.out.println("第二個Java程式");
+        System.out.println("第三個Java程式");
 
     }
 }
